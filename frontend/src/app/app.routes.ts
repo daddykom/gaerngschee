9eb136fa5f2011aad010ea9d0631535606afd8a0
Charley Collins <charley@collins.ch>
2026-06-22 20:09:15 +0200

    CI

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,14 +3,19 @@ import { Routes } from '@angular/router';
 export const appRoutes: Routes = [
   {
     path: '',
-    redirectTo: '/offers',
+    redirectTo: '/offers/list',
     pathMatch: 'full',
   },
   {
     path: 'offers',
+    redirectTo: '/offers/list',
+    pathMatch: 'full',
+  },
+  {
+    path: 'offers/list',
     loadComponent: () =>
       import('./features/offers/offer-list/offer-list.container.component').then(
-        (m) => m.OfferListContainerComponent
+        (m) => m.OfferListContainerComponent,
       ),
   },
 ];
--- frontend/src/app/features/categories/category-list/category-list.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-
-@Component({
-  imports: [MatCardModule],
-  templateUrl: './category-list.component.html',
-})
-export class CategoryListComponent {}
