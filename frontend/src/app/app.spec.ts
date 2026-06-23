810180eb1a8a6077c13928daea0ebc83c483eb7a
Charley Collins <charley@collins.ch>
2026-06-23 23:05:30 +0200

    Test-Fehler

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -1,6 +1,6 @@
+import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 import { NxWelcome } from './nx-welcome';
-import { TestBed } from '@angular/core/testing';
 
 describe('App', () => {
   beforeEach(async () => {
@@ -13,6 +13,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('gaerngschee');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Gratisangebote');
   });
 });
